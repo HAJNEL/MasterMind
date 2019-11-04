7eacba4c5fc1fcb0522072d594fa5fd3de620b79
HAJNEL <hajnel20@gmail.com>
2019-11-05 00:05:37 +0200

    Project Startup
    Added change color function

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {ModalController} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(public modalController: ModalController) {}
 
-  constructor() {}
+  ToggleColorSelector() {
+    const elm =  document.querySelector('.ColorSelector') as HTMLElement;
+    if (elm.style.visibility === 'collapse') {
+      elm.classList.remove('animated', 'bounceOut');
+      elm.style.visibility =  'visible';
+      elm.classList.add('animated', 'bounceIn');
+    } else {
+      elm.classList.remove('animated', 'bounceIn');
+      elm.classList.add('animated', 'bounceOut');
+      elm.style.visibility = 'collapse';
+    }
+  }
 
+  ChangeColor(Color) {
+    const elm =  document.querySelector('.SelectedColor') as HTMLElement;
+    switch (Color) {
+      case 'primary': elm.attributes[3].nodeValue = 'primary'; break;
+      case 'warning': elm.attributes[3].nodeValue = 'warning'; break;
+      case 'danger': elm.attributes[3].nodeValue = 'danger'; break;
+      case 'dark': elm.attributes[3].nodeValue = 'dark'; break;
+      case 'success': elm.attributes[3].nodeValue = 'success'; break;
+    }
+  }
 }
